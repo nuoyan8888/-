5810fb2f3bd7fa142d83a0891d8d27c709793ed4
nuoyan8888 <1077566265@qq.com>
2022-03-15 17:50:45 +0800

    2 files modified

--- JsStudy/jstest.js
+++ JsStudy/jstest.js
@@ -1,3 +1,4 @@
+/*
 const arr = [
   {
     id: 1,
@@ -61,17 +62,59 @@ newArr.push({
 });
 console.log(newArr);
 
-class Test{
-  constructor(num){
-    this.num = num;    
+class Test {
+  constructor(num) {
+    this.num = num;
   }
-  add(num){
-    this.num += num
+  add(num) {
+    this.num += num;
   }
-  log(){
-    console.log(this.num)
+  log() {
+    console.log(this.num);
   }
 }
 const t = new Test(1);
 t.add(2);
 t.log();
+*/
+
+let arr = [];
+function testFn(num) {
+  for (i = 1; i <= num; i++) {
+    if (i <= 2) {
+      arr.push({
+        id: i,
+        code: 'PS' + i,
+        org: '华北配送中心',
+        operator: '操作人' + i,
+        operateTime: '2020/09/08 16:32',
+      });
+    } else {
+      arr.push({
+        id: i,
+        code: 'PS' + i,
+        org: '华南配送中心',
+        operator: '操作人' + i,
+        operateTime: '2020/09/08 16:32',
+      });
+    }
+  }
+  console.log(arr);
+}
+testFn(5);
+
+const thead = document.querySelector('thead');
+thead.innerHTML = '<tr><th>序号</th>';
+thead.innerHTML = thead.innerHTML + '<th>编码</th>';
+thead.innerHTML = thead.innerHTML + '<th>机构</th>';
+thead.innerHTML = thead.innerHTML + '<th>操作人</th>';
+thead.innerHTML = thead.innerHTML + '<th>操作时间</th></tr>';
+for (i = 0; i < arr.length; i++) {
+  const tbody = document.querySelector('tbody');
+  tbody.innerHTML = tbody.innerHTML + '<tr><td>' + arr[i].id + '</td>';
+  tbody.innerHTML = tbody.innerHTML + '<td>' + arr[i].code + '</td>';
+  tbody.innerHTML = tbody.innerHTML + '<td>' + arr[i].org + '</td>';
+  tbody.innerHTML = tbody.innerHTML + '<td>' + arr[i].operator + '</td>';
+  tbody.innerHTML =
+    tbody.innerHTML + '<td>' + arr[i].operateTime + '</td></tr>';
+}
